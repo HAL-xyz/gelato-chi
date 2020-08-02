2c1509c7bc258bbb967da181e39e1d999087135b
gitpusha <gitpusha@gmail.com>
2020-08-02 11:06:17 +0200

    unprovide funds and transfer from gnosis safe

--- rinkeby-test/4-transfer-funds-from-GnosisSafe.js
+++ rinkeby-test/4-transfer-funds-from-GnosisSafe.js
@@ -13,11 +13,7 @@ const CPK = require("contract-proxy-kit");
 // => only dependency we need is "chaFi"
 const { expect } = require("chai");
 
-const GelatoCoreLib = require("@gelatonetwork/core");
-
-const GELATO = bre.network.config.deployments.GelatoCore;
-
-describe("Unproviding ETH deposited on Gelato via GnosisSafe", function () {
+describe("Transfering ETH out of GnosisSafe", function () {
   // No timeout for Mocha due to Rinkeby mining latency
   this.timeout(0);
 
@@ -29,8 +25,6 @@ describe("Unproviding ETH deposited on Gelato via GnosisSafe", function () {
   // 2) We will deploy a GnosisSafeProxy using the Factory, or if we already deployed
   //  one, we will use that one.
   let cpk;
-
-  let gelatoCore;
 
   before(async function () {
     // We get our User Wallet from the Buidler Runtime Env
@@ -47,19 +41,9 @@ describe("Unproviding ETH deposited on Gelato via GnosisSafe", function () {
       console.error("\n CPK.create error ❌  \n", error);
       process.exit(1);
     }
-
-    gelatoCore = await ethers.getContractAt(
-      GelatoCoreLib.GelatoCore.abi,
-      network.config.deployments.GelatoCore // the Rinkeby Address of the deployed GelatoCore
-    );
   });
 
-  it("Withdraws funds from Gelato and transfer to User", async function () {
-    const fundsOnGelato = await gelatoCore.providerFunds(cpk.address);
-    console.log(
-      `Current funds on Gelato: ${utils.formatEther(fundsOnGelato)} ETH`
-    );
-
+  it("Transfer funds from GnosisSafe", async function () {
     const prevFundsInUserProxy = await ethers.provider.getBalance(cpk.address);
     console.log(
       `Current funds in GnosisSafe: ${utils.formatEther(
@@ -67,50 +51,36 @@ describe("Unproviding ETH deposited on Gelato via GnosisSafe", function () {
       )} ETH`
     );
 
-    if (fundsOnGelato.eq("0")) {
+    if (prevFundsInUserProxy.eq("0")) {
       console.log(
-        `❌ GnosisSafe ${cpk.address} has no funds on Gelato on ${bre.network.name}`
+        `❌ GnosisSafe ${cpk.address} has no funds on ${bre.network.name}`
       );
       process.exit(1);
     }
 
-    console.log(`\n Withdrawing ${utils.formatEther(fundsOnGelato)} ETH`);
+    console.log(
+      `\n Transferring ${utils.formatEther(
+        prevFundsInUserProxy
+      )} ETH to ${myUserAddress} on ${bre.network.name}`
+    );
     try {
       const tx = await cpk.execTransactions([
         {
           operation: CPK.CALL,
-          to: GELATO,
-          value: 0,
-          data: await bre.run("abi-encode-withselector", {
-            abi: GelatoCoreLib.GelatoCore.abi,
-            functionname: "unprovideFunds",
-            inputs: [fundsOnGelato],
-          }),
-        },
-        {
-          operation: CPK.CALL,
           to: myUserAddress,
-          value: fundsOnGelato,
+          value: prevFundsInUserProxy,
           data: "0x",
         },
       ]);
       // Wait for mining
-      // await tx.wait()  <==== somehow this doesnt work
       console.log(`Tx Hash: ${tx.hash}`);
 
-      const newFundsOnGelato = await gelatoCore.providerFunds(cpk.address);
-      expect(newFundsOnGelato).to.be.equal(0);
-      console.log(
-        `New funds in Gelato: ${utils.formatEther(newFundsOnGelato)} ETH`
-      );
-
       const fundsInUserProxy = await ethers.provider.getBalance(cpk.address);
-      expect(fundsInUserProxy).to.be.equal(prevFundsInUserProxy);
-      console.log(
-        `Funds in GnosisSafe: ${utils.formatEther(fundsInUserProxy)} ETH`
-      );
+      expect(fundsInUserProxy).to.be.equal(0);
+      console.log(`New funds in GnosisSafe at ${cpk.address}`);
+      console.log(`${utils.formatEther(fundsInUserProxy)} ETH`);
     } catch (error) {
-      console.error("\n Gelato unprovideFunds error ❌  \n", error);
+      console.error("\n GnosisSafe transfer funds error ❌  \n", error);
       process.exit(1);
     }
   });
