c6ef2639705e65e1d9dea8967a434db16e4dfda4
gitpusha <gitpusha@gmail.com>
2020-08-04 08:55:12 +0200

    0.5 sol version of CPKFactoryCustom

--- contracts/CPK/CPKFactoryCustom.sol
+++ contracts/CPK/CPKFactoryCustom.sol
@@ -1,17 +1,17 @@
 // https://github.com/gnosis/contract-proxy-kit/blob/master/contracts/CPKFactory.sol
 // Adjusted to fix bug: https://github.com/gnosis/contract-proxy-kit/issues/90
 // Adjusted to have payable modifier
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity 0.5.12;
 
-import { Enum } from "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
-import { Proxy } from "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
-import { GnosisSafe } from "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+import {Enum} from "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+import {GnosisSafeProxy} from "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
+import {GnosisSafe} from "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
 
 contract CPKFactoryCustom {
-    event ProxyCreation(Proxy proxy);
+    event ProxyCreation(GnosisSafeProxy proxy);
 
     function proxyCreationCode() external pure returns (bytes memory) {
-        return type(Proxy).creationCode;
+        return type(GnosisSafeProxy).creationCode;
     }
 
     function createProxyAndExecTransaction(
@@ -29,7 +29,7 @@ contract CPKFactoryCustom {
     {
         GnosisSafe proxy;
         bytes memory deploymentData = abi.encodePacked(
-            type(Proxy).creationCode,
+            type(GnosisSafeProxy).creationCode,
             abi.encode(masterCopy)
         );
         bytes32 salt = keccak256(abi.encode(msg.sender, saltNonce));
@@ -46,8 +46,8 @@ contract CPKFactoryCustom {
             tmp[0] = address(this);
 
             // Setup Proxy with CPKFactory as owner
-            try proxy.setup(
-                tmp,
+            proxy.setup(
+                tmp,  // [CPKFactory]
                 1,
                 address(0),
                 "",
@@ -55,18 +55,11 @@ contract CPKFactoryCustom {
                 address(0),
                 0,
                 address(0)
-            ) {
-            } catch Error(string memory error) {
-                revert(
-                    string(abi.encodePacked("CPKFactoryCustom.create.setup:", error))
-                );
-            } catch {
-                revert("CPKFactoryCustom.create.setup:unknown error");
-            }
+            );
         }
 
         // Exec arbitrary Logic (could use multisend)
-        try proxy.execTransaction{value: msg.value}(
+        execTransactionSuccess = proxy.execTransaction.value(msg.value)(
             to,
             value,
             data,
@@ -77,18 +70,11 @@ contract CPKFactoryCustom {
             address(0),
             address(0),
             abi.encodePacked(uint(address(this)), uint(0), uint8(1))
-        ) returns (bool success) {
-            execTransactionSuccess = success;
-        } catch Error(string memory error) {
-            revert(
-                string(abi.encodePacked("CPKFactoryCustom.create.execTransaction:", error))
-            );
-        } catch {
-            revert("CPKFactoryCustom.create.execTransaction:unknown error");
-        }
+        );
+        require(execTransactionSuccess, "CPKFactoryCustom.create.execTransaction: failed");
 
         // SwapOwner from CPKFactory to msg.sender
-        try proxy.execTransaction(
+        execTransactionSuccess = proxy.execTransaction(
             address(proxy), 0,
             abi.encodeWithSignature(
                 "swapOwner(address,address,address)",
@@ -103,14 +89,9 @@ contract CPKFactoryCustom {
             address(0),
             address(0),
             abi.encodePacked(uint(address(this)), uint(0), uint8(1))
-        ) returns (bool success) {
-            emit ProxyCreation(Proxy(address(proxy)));
-        } catch Error(string memory error) {
-            revert(
-                string(abi.encodePacked("CPKFactoryCustom.create.swapOwner:", error))
-            );
-        } catch {
-            revert("CPKFactoryCustom.create.swapOwner:unknown error");
-        }
+        );
+        require(execTransactionSuccess, "CPKFactoryCustom.create.swapOwner: failed");
+
+        emit ProxyCreation(GnosisSafeProxy(address(proxy)));
    }
 }
